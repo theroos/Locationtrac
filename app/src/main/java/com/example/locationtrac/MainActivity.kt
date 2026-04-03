bcc579fbc7dbf4e88004b900be49a7e2d4360fa8
theroos <roosonline7@gmail.com>
2026-04-04 01:41:46 +0530

    Location tracking.

---
+++ app/src/main/java/com/example/locationtrac/MainActivity.kt
@@ -0,0 +1,145 @@
+package com.example.locationtrac
+
+import android.Manifest
+import androidx.compose.ui.tooling.preview.Preview
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.os.Bundle
+import android.os.Looper
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.example.locationtrac.ui.theme.LocationTracTheme
+import com.google.android.gms.location.*
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var locationCallback: LocationCallback
+    private val districtState = mutableStateOf("Detecting District...")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
+        fusedLocationProviderClient =
+            LocationServices.getFusedLocationProviderClient(this)
+
+        setContent {
+            LocationTracTheme {
+                DistrictScreen(districtState.value)
+            }
+        }
+
+        // Permission check
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            startLocationUpdates()
+        } else {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                100
+            )
+        }
+    }
+
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == 100 &&
+            grantResults.isNotEmpty() &&
+            grantResults[0] == PackageManager.PERMISSION_GRANTED
+        ) {
+            startLocationUpdates()
+        }
+    }
+
+    @RequiresPermission(allOf = [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION])
+    private fun startLocationUpdates() {
+
+        val locationRequest = LocationRequest.Builder(
+            Priority.PRIORITY_HIGH_ACCURACY,
+            5000
+        ).build()
+
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(result: LocationResult) {
+                for (location in result.locations) {
+                    val district = getDistrictName(
+                        location.latitude,
+                        location.longitude
+                    )
+                    districtState.value = district
+                }
+            }
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(
+            locationRequest,
+            locationCallback,
+            Looper.getMainLooper()
+        )
+    }
+
+    
+    private fun getDistrictName(lat: Double, long: Double): String {
+        return try {
+            val geocoder = Geocoder(this, Locale.getDefault())
+            val addresses = geocoder.getFromLocation(lat, long, 1)
+
+            if (!addresses.isNullOrEmpty()) {
+                addresses[0].subAdminArea ?: "Unknown District"
+            } else {
+                "Unknown District"
+            }
+        } catch (e: Exception) {
+            "Error Fetching District"
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+    }
+}
+
+
+@Composable
+fun DistrictScreen(district: String) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "You are currently at $district",
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DistrictScreenPreview() {
+    DistrictScreen("Kolkata")
+}
